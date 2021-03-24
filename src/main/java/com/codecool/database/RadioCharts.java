e7260881b0a6c9017fb130981d98a00bd4da540b
adamczego <cz4d4m@gmail.com>
2021-03-24 17:29:54 +0100

    complete task

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,97 @@
 package com.codecool.database;
 
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RadioCharts {
-    //TODO: implement this class
+
+    private final String DB_URL;
+    private final String DB_USER;
+    private final String DB_PASSWORD;
+
+
+    public RadioCharts(String DB_URL, String DB_USER, String DB_PASSWORD) {
+        this.DB_URL = DB_URL;
+        this.DB_USER = DB_USER;
+        this.DB_PASSWORD = DB_PASSWORD;
+    }
+
+    public String getMostPlayedSong() {
+
+        List<Song> songs = new ArrayList<>();
+
+        String q =
+            "SELECT * " +
+            "FROM music_broadcast ORDER BY times_aired DESC;";
+
+        try(Connection connection = this.getConnection();
+            PreparedStatement ps = connection.prepareStatement(q)) {
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()){
+
+                songs.add(new Song(rs.getString(2), rs.getInt(3)));
+
+            }
+
+        } catch (SQLException e) {
+            return "";
+        }
+
+        songs.forEach(x -> System.out.println(x.getTitle()));
+
+        return songs.isEmpty()
+            ? ""
+            : songs.get(0).getTitle();
+
+    }
+
+
+    public String getMostActiveArtist() {
+
+        List<Artist> artists = new ArrayList<>();
+
+        String q =
+            "SELECT *" +
+            "FROM music_broadcast;";
+
+        try(Connection connection = this.getConnection();
+            PreparedStatement ps = connection.prepareStatement(q)) {
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()){
+
+                Artist newArtist = new Artist(rs.getString(1));
+                newArtist.addSongTitle(rs.getString(1));
+                artists.add(newArtist);
+
+            }
+
+        } catch (SQLException e) {
+            return "";
+        }
+
+        return artists.isEmpty()
+            ? ""
+            : artists.get(0).getName();
+
+    }
+
+
+    public Connection getConnection() throws SQLException {
+
+        Connection connection;
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        } catch (SQLException e) {
+            throw new SQLException();
+        }
+
+        return connection;
+    }
 }
